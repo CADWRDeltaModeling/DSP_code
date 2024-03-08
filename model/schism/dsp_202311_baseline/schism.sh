ff317f91999110e6195a4b2fb930739c4ca92a4c
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2024-03-07 17:15:59 -0800

    correcting inputs and cleaning repo

--- model/schism/dsp_202311_baseline/schism.sh
+++ model/schism/dsp_202311_baseline/schism.sh
@@ -1,14 +1,8 @@
-#!/bin/sh
-# Purge any modules that are already loaded.
+#!/bin/bash
 module purge
-# Load intel compilers
-module use /opt/intel/oneapi/modulefiles
-module load compiler/2022.1.0
-module load mpi/2021.5.1
-
-# Load dependencies: HDF5, NetCDF
-module load hdf5/1.12.2_intel2022.1
-module load netcdf-c/4.9.0_intel2022.1
-module load netcdf-fortran/4.6.0_intel2022.1
+module load intel/2024.0 openmpi/5.0.2 hdf5/1.14.3 netcdf-c/4.9.2 netcdf-fortran/4.6.1 schism/5.11.0
+ 
 ulimit -s unlimited
-/home/tomkovic/schism_build/build_20220927/bin/pschism_PREC_EVAP_GOTM_TVD-VL 10
+NSCRIBES=10
+SCHISM_BIN=pschism_PREC_EVAP_GOTM_TVD-VL
+$SCHISM_BIN $NSCRIBES
