0db09a78ece6727277a833b0130670e342427bca
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2024-03-20 15:18:17 -0700

    fix input options

--- model/schism/dsp_202311_baseline/slurm_batch_file.sh
+++ model/schism/dsp_202311_baseline/slurm_batch_file.sh
@@ -3,10 +3,10 @@
 #SBATCH --partition=work        # Partition name to submit the job
 #SBATCH --mail-type=END,FAIL            # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=lily.tomkovic@water.ca.gov       # Where to send mail
-#SBATCH --ntasks=192                      # Number of MPI ranks (or cores)
-#SBATCH --nodes=6                       # Number of nodes
+#SBATCH --ntasks=192                      # Number of MPI ranks (or cores) (192)
+#SBATCH --nodes=6                       # Number of nodes (6)
 #SBATCH --ntasks-per-node=32             # How many tasks on each node
-#SBATCH --output=itp202311_%j.log     # Standard output and error log
+#SBATCH --output=dsp_2024_%j.log     # Standard output and error log
 
 module load intel/2024.0
 module load openmpi/5.0.2
