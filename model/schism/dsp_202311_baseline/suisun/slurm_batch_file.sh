42ffade8a9e124f8716be85ee9333739f6fa2903
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2024-04-03 11:43:49 -0700

    update suisun inputs

--- model/schism/dsp_202311_baseline/suisun/slurm_batch_file.sh
+++ model/schism/dsp_202311_baseline/suisun/slurm_batch_file.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=192                      # Number of MPI ranks (or cores)
 #SBATCH --nodes=6                       # Number of nodes
 #SBATCH --ntasks-per-node=32             # How many tasks on each node
-#SBATCH --output=itp202311_%j.log     # Standard output and error log
+#SBATCH --output=dsp2024_%j.log     # Standard output and error log
 
 module load intel/2024.0
 module load openmpi/5.0.2
