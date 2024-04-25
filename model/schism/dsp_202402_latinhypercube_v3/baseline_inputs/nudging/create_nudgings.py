fe9c027c1efe928ee5333deee43cda2b1f739d41
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2024-04-25 15:19:00 -0700

    generated a combination of sources for 2014 year

--- model/schism/dsp_202402_latinhypercube_v3/baseline_inputs/nudging/create_nudgings.py
+++ model/schism/dsp_202402_latinhypercube_v3/baseline_inputs/nudging/create_nudgings.py
@@ -35,7 +35,7 @@ def make_nudging_nc(yaml_fn, date_range, interp_source, model_dir):
     ndge.create_nudging()
     
     # delete temp_yml
-    os.remove(temp_yml)
+    # os.remove(temp_yml)
 
 
 if __name__ == '__main__':
@@ -63,7 +63,7 @@ if __name__ == '__main__':
                 interp_dict[year] = ['sfb_forecast/processed/ca_subSFB_fcst_hourly_pst',
                                      'cencoos_ca_roms/processed/cencoos_hourly_pst']
                 date_range_dict[year] = [[cp['case_start'], datetime.date(2014,1,1)], 
-                                     [datetime.date(2014,5,1), cp['case_end']]] # set the model run period based on lhc setup file
+                                     [datetime.date(2014,1,2), cp['case_end']]] # set the model run period based on lhc setup file
 
     model_dir = os.path.join(os.getcwd(),'../')
     yaml_fn = os.path.join(os.getcwd(),"./nudging_fmt.yaml")
@@ -80,16 +80,29 @@ if __name__ == '__main__':
             date_range = date_ranges
             interp_source = interp_dict[year][0]
             make_nudging_nc(yaml_fn, date_range, interp_source, model_dir)
+            print(f'writing nudging files for {year}')
 
         else:
             # the simulation period spans two interpolant data sources, need to run two different nudging calls
+            print(f'Need to create combined netCDF for {year}')
+            interp_dirs = []
             for i, interp_source in enumerate(interp_dict[year]):
                 interp_dir = interp_source.split('/')[0]
+                interp_dirs.append(interp_dir)
                 if not os.path.exists(interp_dir):
                     os.mkdir(interp_dir)
                 os.chdir(interp_dir)
                 date_range = date_ranges[i]
                 make_nudging_nc(yaml_fn, date_range, interp_source, model_dir)
                 os.chdir('../')
+
+            # TODO: stitch together two nudging netCDF files
+            for var in ['TEM','SAL']:
+                nc1 = xr.open_dataset(os.path.join(interp_dirs[0],f'{var}_nu_roms.nc'))
+                nc2 = xr.open_dataset(os.path.join(interp_dirs[1],f'{var}_nu_roms.nc'))
+                nc2.assign(time=nc2.time + ((date_ranges[1][0]-date_ranges[0][0]).days * 86400)) # shift the second time dataset by the difference between datasteps
+                ncout = xr.concat([nc1,nc2], dim='time')
+                ncout.to_netcdf(f'{var}_nu_roms.nc')
+                print(f"Wrote combined netCDF: {var}_nu_roms.nc")
         
         os.chdir('../')
