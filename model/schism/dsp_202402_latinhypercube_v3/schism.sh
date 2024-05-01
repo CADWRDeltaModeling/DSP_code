1a686dcabd58e7bd4c86ba24d59cdac382967eed
lily-tomkovic-DWR <lily.tomkovic@water.ca.gov>
2024-05-01 12:55:44 -0700

    use ucx compiler/import modules

--- model/schism/dsp_202402_latinhypercube_v3/schism.sh
+++ model/schism/dsp_202402_latinhypercube_v3/schism.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module purge
-module load intel/2024.0 openmpi/5.0.2 hdf5/1.14.3 netcdf-c/4.9.2 netcdf-fortran/4.6.1 schism/5.11.0
+module load intel/2024.0 ucx/1.16.0 openmpi/5.0.2-ucx hdf5/1.14.3 netcdf-c/4.9.2 netcdf-fortran/4.6.1 schism/5.11.0
  
 ulimit -s unlimited
 NSCRIBES=10
