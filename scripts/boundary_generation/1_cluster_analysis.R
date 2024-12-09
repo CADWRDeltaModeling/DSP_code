e19fc177d96913e084afb838c0e9ed02b2d5facd
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2024-12-09 15:12:02 -0800

    updated latinhypercube v4

--- scripts/boundary_generation/1_cluster_analysis.R
+++ scripts/boundary_generation/1_cluster_analysis.R
@@ -17,6 +17,7 @@ library(reshape2)
 library(hash)
 library(stringr)
 library(ggh4x)
+library(htmlwidgets)
 library(scales)
 
 rm(list=ls(all=TRUE)) #start with empty workspace
@@ -30,9 +31,10 @@ source("./functions/load_delta_vars.R")
 # DSM2 Inputs/Outputs for ANN
 dsm2_filename <- '../../data/dsm2_ann_inputs_base.xlsx'
 # ec outputs are reported in micro-Siemens/cm. 1000 mS/cm = 1 mmhos/cm
-
+# sheetlist <- excel_sheets(path=dsm2_filename)
 northern_flow <- load_delta_vars(dsm2_filename, 'northern_flow')
 exports <- load_delta_vars(dsm2_filename, 'exports')
+ec_outputs <- load_delta_vars(dsm2_filename, 'base_ec_output')
 
 cols <- c("navy","royalblue1","mediumorchid","orangered3","palegreen3","goldenrod","green4","paleturquoise1","yellow1","pink1","slateblue2")
 col.clust <- data.frame(clust=c(1:length(cols)), color=cols)
@@ -49,6 +51,8 @@ y.scale <- facetted_pos_scales(y=list(
   scale_y_log10(breaks=flow.brks, labels=comma(flow.brks, accuracy=1))))
 
 plt.out.dir <- 'plots'
+
+ec_eval_locs <- c("RSAN018-JERSEYPOINT","RSAC081-COLLINSVILLE","RSAC092-EMMATON","RSAN007-ANTIOCH","ROLD024-OLD RIVER AT BACON ISLAND","CHSWP003-CCFB_INTAKE")
 
 # Pre-prepare Data --------------------------------------------------------
 
@@ -69,7 +73,7 @@ nfex.decdry.rshp <- merge(nfl.dec.dry.rshp, exp.dec.dry.rshp, by='wy') # columns
 nf.dd.norm <- norm.biv.df(nfex.decdry.rshp, 2)
 nf.dd.norm.stat <- nf.dd.norm[,!(names(nf.dd.norm) %in% c('wy'))]
 
-remove(var, error=FALSE)
+
 wss <- (nrow(nf.dd.norm.stat)-1)*sum(apply(nf.dd.norm.stat,2,var))
 for (i in 2:15) wss[i] <- sum(kmeans(nf.dd.norm.stat,centers=i)$withinss)
 x11()
@@ -84,130 +88,133 @@ dev.off()
 
 # NOTICE: Looks as though 4 clusters would be most appropriate when including May in analysis
 
-# K-means clustering with 4 -----------------------------------------------
-
-
-nclust <- 4 # Using 4 4 might be most appropriate
-bivars <- c('Flow','Export')
-nfex.decdry4 <- clust.plt(nclust, nf.dd.norm.stat, nfex.full.rshp, plt.out.dir, refdate=refdate, bivars=bivars, y.scale=y.scale, only.clust.legend=TRUE) # cluster with nfex.dd.norm.stat but plot with nfex.full.rshp (un-log-normalized full time series for plotting)
-nfex.decdry4.km <- nfex.decdry4$km.cb
-
-# save(nfex.decdry4.km, file=paste0('output/',plt.out.dir,'/nfexdecdry4km.RData'))
-
-# Determine selected water years ------------------------------------------
-
-slct.df <- nf.dd.norm
-bivars <- c('Flow','Export')
-obs <- ncol(slct.df) - 1 # number of days times number of bivariates
-clusts <- nfex.decdry4.km$partition
-
-wy.metrics <- data.frame(wy=slct.df$wy, clust=clusts, skill=NA, rmse=NA, mmskill=NA, mmrmse=NA)
-
-slct.df$clust <- clusts
-jds <- names(slct.df)[!(names(slct.df) %in% c('wy','clust'))]
-for (c in sort(unique(clusts))) {
-  
-  # get average values for cluster
-  mean.clust <- data.frame(jday=jds, mean=NA)
-  for (jd in jds) {
-    mean.clust$mean[mean.clust$jday==jd] <- mean(slct.df[slct.df$clust==c,names(slct.df) %in% c(jd)])
-  } # end jd mean loop
-  
-  # compute metrics for each wy within cluster
-  for (wy in slct.df$wy[slct.df$clust==c]) {
-    wy.df <- data.frame(jday=jds,
-                        pred=unlist(slct.df[slct.df$wy==wy, jds]))
-    wy.df <- merge(wy.df, mean.clust, by='jday')
-    wy.metrics$rmse[wy.metrics$wy==wy] <- sqrt(sum((wy.df$pred-wy.df$mean)^2)/nrow(wy.df))
-    wy.metrics$skill[wy.metrics$wy==wy] <- 1-(sum((wy.df$pred-wy.df$mean)^2)/
-                                                (sum((abs(wy.df$pred-mean(wy.df$mean))+abs(wy.df$mean-mean(wy.df$mean)))^2)))
-  } # end wy loop
-  
-  # compute max and min skill/rmse
-  wy.metrics$mmskill[wy.metrics$skill==
-                       max(wy.metrics$skill[wy.metrics$clust==c & wy.metrics$wy > 2007], na.rm=TRUE)] <- paste0('Max Skill cluster ', c)
-  wy.metrics$mmskill[wy.metrics$skill==
-                       min(wy.metrics$skill[wy.metrics$clust==c], na.rm=TRUE)] <- paste0('Min Skill cluster ', c)
-  wy.metrics$mmrmse[wy.metrics$rmse==
-                      max(wy.metrics$rmse[wy.metrics$clust==c], na.rm=TRUE)] <- paste0('Max RMSE cluster ', c)
-  wy.metrics$mmrmse[wy.metrics$rmse==
-                      min(wy.metrics$rmse[wy.metrics$clust==c & wy.metrics$wy > 2007], na.rm=TRUE)] <- paste0('Min RMSE cluster ', c)
-  
-} # end cluster loop
-
-wy.metrics <- wy.metrics[order(wy.metrics$clust, wy.metrics$rmse),]
-wy.metrics[grepl("Min RMSE",wy.metrics$mmrmse),]
-wy.slcts <- wy.metrics[grepl("Min RMSE",wy.metrics$mmrmse),'wy']
-wy.slcts <- wy.slcts[!(wy.slcts %in% c(2019))] # chose to only include 2017 and not 2019 in cluster 6 to keep limited to 6 years
-sort(wy.slcts) 
-
-# save(wy.metrics, file=paste0('output/',plt.out.dir,'/wymetrics_cluster.RData'))
-
-df.plt.rshp <- nfex.decdry4$df.plt.rshp
-plt.cst.rshp <- nfex.decdry4$plt.cst.rshp
-plots <- c('Min Skill','Max Skill','Min RMSE','Max RMSE')
-
-wy.to.clust <- slct.df[,c('wy','clust')]
-# save(wy.to.clust, file=paste0('output/',plt.out.dir,'/wytoclust.RData'))
-
-# Plot final selection ----------------------------------------------------
-
-slct.wys <- wy.slcts
-
-wycol <- merge(wy.to.clust, col.clust, by='clust')
-unslcted <- sort(unique(df.plt.rshp$variable)[!(unique(df.plt.rshp$variable) %in% slct.wys)])
-c.colordf <- data.frame(breaks=c(unslcted, 
-                                 wycol$wy[wycol$wy %in% slct.wys]),
-                        values=c(rep('grey',length(unslcted)),
-                                 wycol$color[wycol$wy %in% slct.wys]))
-flow.brks <- c(5000,10000, 20000, 30000, 40000, 50000,100000)
-figname <-  paste0(plt.out.dir,'/Final_WY_Selection.png')
-
-# create facet wrap plot for all years with the selected years plotted in respective cluster color
-plt <- ggplot() +
-  geom_line(data=df.plt.rshp, mapping=aes(x=dt,y=value,color=variable), linewidth=0.5, alpha=0.5, show.legend=FALSE) +
-  geom_line(data=df.plt.rshp[df.plt.rshp$variable %in% slct.wys,], mapping=aes(x=dt,y=value,color=variable), linewidth=1) +
-  facet_wrap(~bivar, nrow=length(bivars), scales='free') +
-  scale_color_manual(breaks= c.colordf$breaks,
-                     values=c.colordf$values,
-                     # guide=guide_legend(override.aes=list(linetype=0)),
-                     name="Cluster") +
-  # scale_y_continuous(limits=c(0,500000), breaks=seq(0,500000,100000), expand=c(0,0)) +
-  # scale_y_continuous(limits=c(0,25000), expand=c(0,0)) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b", expand=c(0,0)) +
-  labs(x='',y='Discharge (cfs)') +
-  guides(color=guide_legend(nrow=2)) + 
-  facetted_pos_scales(y=list(
-    scale_y_continuous(limits=c(0,14000), expand=c(0,0)),
-    scale_y_log10(breaks=flow.brks, labels=comma(flow.brks, accuracy=1)))) +
-  theme(text=element_text(size=12),
-        panel.border=element_rect(colour = "black", fill=NA, linewidth=0.5),
-        panel.background = element_blank(),
-        # legend.key=element_blank(),
-        panel.grid.major.y = element_line(size=.25, colour='grey80', linetype = 'dashed'),
-        axis.line = element_line(colour = "black"),
-        # legend.position=c(0.975,0.975),
-        legend.position='bottom',
-        legend.box='vertical',
-        # legend.justification=c(0.975,0.975),
-        legend.spacing=unit(c(0,0,0,0),"null"),
-        legend.background = element_rect(fill = "white", color = NULL),
-        # legend.title=element_blank(),
-        axis.title.y = element_text(color='black'),
-        axis.text.x = element_text(angle=90))
-plt
-ggsave(figname, width=12.5, height=7)
-
+# # K-means clustering with 4 -----------------------------------------------
+# 
+# 
+# nclust <- 4 # Using 4 4 might be most appropriate
+# bivars <- c('Flow','Export')
+# nfex.decdry4 <- clust.plt(nclust, nf.dd.norm.stat, nfex.full.rshp, plt.out.dir, refdate=refdate, bivars=bivars, y.scale=y.scale, only.clust.legend=TRUE) # cluster with nfex.dd.norm.stat but plot with nfex.full.rshp (un-log-normalized full time series for plotting)
+# nfex.decdry4.km <- nfex.decdry4$km.cb
+# 
+# # save(nfex.decdry4.km, file=paste0('output/',plt.out.dir,'/nfexdecdry4km.RData'))
+# 
+# # Determine selected water years ------------------------------------------
+# 
+# slct.df <- nf.dd.norm
+# bivars <- c('Flow','Export')
+# obs <- ncol(slct.df) - 1 # number of days times number of bivariates
+# clusts <- nfex.decdry4.km$partition
+# 
+# wy.metrics <- data.frame(wy=slct.df$wy, clust=clusts, skill=NA, rmse=NA, mmskill=NA, mmrmse=NA)
+# 
+# slct.df$clust <- clusts
+# jds <- names(slct.df)[!(names(slct.df) %in% c('wy','clust'))]
+# for (c in sort(unique(clusts))) {
+#   
+#   # get average values for cluster
+#   mean.clust <- data.frame(jday=jds, mean=NA)
+#   for (jd in jds) {
+#     mean.clust$mean[mean.clust$jday==jd] <- mean(slct.df[slct.df$clust==c,names(slct.df) %in% c(jd)])
+#   } # end jd mean loop
+#   
+#   # compute metrics for each wy within cluster
+#   for (wy in slct.df$wy[slct.df$clust==c]) {
+#     wy.df <- data.frame(jday=jds,
+#                         pred=unlist(slct.df[slct.df$wy==wy, jds]))
+#     wy.df <- merge(wy.df, mean.clust, by='jday')
+#     wy.metrics$rmse[wy.metrics$wy==wy] <- sqrt(sum((wy.df$pred-wy.df$mean)^2)/nrow(wy.df))
+#     wy.metrics$skill[wy.metrics$wy==wy] <- 1-(sum((wy.df$pred-wy.df$mean)^2)/
+#                                                 (sum((abs(wy.df$pred-mean(wy.df$mean))+abs(wy.df$mean-mean(wy.df$mean)))^2)))
+#   } # end wy loop
+#   
+#   # compute max and min skill/rmse
+#   wy.metrics$mmskill[wy.metrics$skill==
+#                        max(wy.metrics$skill[wy.metrics$clust==c & wy.metrics$wy > 2007], na.rm=TRUE)] <- paste0('Max Skill cluster ', c)
+#   wy.metrics$mmskill[wy.metrics$skill==
+#                        min(wy.metrics$skill[wy.metrics$clust==c], na.rm=TRUE)] <- paste0('Min Skill cluster ', c)
+#   wy.metrics$mmrmse[wy.metrics$rmse==
+#                       max(wy.metrics$rmse[wy.metrics$clust==c], na.rm=TRUE)] <- paste0('Max RMSE cluster ', c)
+#   wy.metrics$mmrmse[wy.metrics$rmse==
+#                       min(wy.metrics$rmse[wy.metrics$clust==c & wy.metrics$wy > 2007], na.rm=TRUE)] <- paste0('Min RMSE cluster ', c)
+#   
+# } # end cluster loop
+# 
+# wy.metrics <- wy.metrics[order(wy.metrics$clust, wy.metrics$rmse),]
+# wy.metrics[grepl("Min RMSE",wy.metrics$mmrmse),]
+# wy.slcts <- wy.metrics[grepl("Min RMSE",wy.metrics$mmrmse),'wy']
+# wy.slcts <- wy.slcts[!(wy.slcts %in% c(2019))] # chose to only include 2017 and not 2019 in cluster 6 to keep limited to 6 years
+# sort(wy.slcts) 
+# 
+# # save(wy.metrics, file=paste0('output/',plt.out.dir,'/wymetrics_cluster.RData'))
+# 
+# df.plt.rshp <- nfex.decdry4$df.plt.rshp
+# plt.cst.rshp <- nfex.decdry4$plt.cst.rshp
+# plots <- c('Min Skill','Max Skill','Min RMSE','Max RMSE')
+# 
+# wy.to.clust <- slct.df[,c('wy','clust')]
+# # save(wy.to.clust, file=paste0('output/',plt.out.dir,'/wytoclust.RData'))
+# 
+# # Plot final selection ----------------------------------------------------
+# 
+# slct.wys <- wy.slcts
+# 
+# wycol <- merge(wy.to.clust, col.clust, by='clust')
+# unslcted <- sort(unique(df.plt.rshp$variable)[!(unique(df.plt.rshp$variable) %in% slct.wys)])
+# c.colordf <- data.frame(breaks=c(unslcted, 
+#                                  wycol$wy[wycol$wy %in% slct.wys]),
+#                         values=c(rep('grey',length(unslcted)),
+#                                  wycol$color[wycol$wy %in% slct.wys]))
+# flow.brks <- c(5000,10000, 20000, 30000, 40000, 50000,100000)
+# figname <-  paste0(plt.out.dir,'/Final_WY_Selection.png')
+# 
+# # create facet wrap plot for all years with the selected years plotted in respective cluster color
+# plt <- ggplot() +
+#   geom_line(data=df.plt.rshp, mapping=aes(x=dt,y=value,color=variable), linewidth=0.5, alpha=0.5, show.legend=FALSE) +
+#   geom_line(data=df.plt.rshp[df.plt.rshp$variable %in% slct.wys,], mapping=aes(x=dt,y=value,color=variable), linewidth=1) +
+#   facet_wrap(~bivar, nrow=length(bivars), scales='free') +
+#   scale_color_manual(breaks= c.colordf$breaks,
+#                      values=c.colordf$values,
+#                      # guide=guide_legend(override.aes=list(linetype=0)),
+#                      name="Cluster") +
+#   # scale_y_continuous(limits=c(0,500000), breaks=seq(0,500000,100000), expand=c(0,0)) +
+#   # scale_y_continuous(limits=c(0,25000), expand=c(0,0)) +
+#   scale_x_date(date_breaks = "1 month", date_labels = "%b", expand=c(0,0)) +
+#   labs(x='',y='Discharge (cfs)') +
+#   guides(color=guide_legend(nrow=2)) + 
+#   facetted_pos_scales(y=list(
+#     scale_y_continuous(limits=c(0,14000), expand=c(0,0)),
+#     scale_y_log10(breaks=flow.brks, labels=comma(flow.brks, accuracy=1)))) +
+#   theme(text=element_text(size=12),
+#         panel.border=element_rect(colour = "black", fill=NA, linewidth=0.5),
+#         panel.background = element_blank(),
+#         # legend.key=element_blank(),
+#         panel.grid.major.y = element_line(size=.25, colour='grey80', linetype = 'dashed'),
+#         axis.line = element_line(colour = "black"),
+#         # legend.position=c(0.975,0.975),
+#         legend.position='bottom',
+#         legend.box='vertical',
+#         # legend.justification=c(0.975,0.975),
+#         legend.spacing=unit(c(0,0,0,0),"null"),
+#         legend.background = element_rect(fill = "white", color = NULL),
+#         # legend.title=element_blank(),
+#         axis.title.y = element_text(color='black'),
+#         axis.text.x = element_text(angle=90))
+# plt
+# ggsave(figname, width=12.5, height=7)
+# 
 
 # K-means clustering with N# -----------------------------------------------
 
 
 nclust <- 10 # Using 10 for greater variability in larger DSM2 dataset
 bivars <- c('Flow','Export')
-nfex.decdry10 <- clust.plt(nclust, nf.dd.norm.stat, nfex.full.rshp, plt.out.dir, refdate=refdate, bivars=bivars, y.scale=y.scale, only.clust.legend=TRUE) # cluster with nfex.dd.norm.stat but plot with nfex.full.rshp (un-log-normalized full time series for plotting)
-nfex.decdry10.km <- nfex.decdry10$km.cb
+# these have been saved (10 clusters) to the RData file. Just load
+# nfex.decdry10 <- clust.plt(nclust, nf.dd.norm.stat, nfex.full.rshp, plt.out.dir, refdate=refdate, bivars=bivars, y.scale=y.scale, only.clust.legend=TRUE) # cluster with nfex.dd.norm.stat but plot with nfex.full.rshp (un-log-normalized full time series for plotting)
+# nfex.decdry10.km <- nfex.decdry10$km.cb
 
-# save(nfex.decdry10.km, file=paste0('data_out/nfex.decdry10.kmnfex.decdry10.km.RData'))
+# save(list=c('nfex.decdry10', 'nfex.decdry10.km'), file=paste0('data_out/nfex.decdry',nclust,'.RData'))
+
+load(paste0('data_out/nfex.decdry',nclust,'.RData'))
 
 # Determine selected water years for 10 clusters ------------------------------------------
 
@@ -258,8 +265,8 @@ sort(wy.slcts)
 
 # save(wy.metrics, file=paste0('output/',plt.out.dir,'/wymetrics_cluster.RData'))
 
-df.plt.rshp <- nfex.decdry4$df.plt.rshp
-plt.cst.rshp <- nfex.decdry4$plt.cst.rshp
+df.plt.rshp <- nfex.decdry10$df.plt.rshp
+plt.cst.rshp <- nfex.decdry10$plt.cst.rshp
 plots <- c('Min Skill','Max Skill','Min RMSE','Max RMSE')
 
 wy.to.clust <- slct.df[,c('wy','clust')]
@@ -313,3 +320,102 @@ plt <- ggplot() +
   ggtitle(paste0(nclust," Clusters"))
 plt
 ggsave(figname, width=12.5, height=7)
+
+
+# Evaluate EC in these windows ---------------------------------------------------------------
+
+
+# selection period used to determine the cluster 
+slct_pers <- data.frame(
+  year= sort(wy.slcts) ,
+  start=lubridate::ymd(paste0(sort(wy.slcts), "-05-01")),
+  end=lubridate::ymd(paste0(sort(wy.slcts), "-11-30"))
+)
+# simulation period windows determined by evaluating the ##_cluster_wy_slct.html plot
+mod_pers <- data.frame(
+  year= sort(wy.slcts), # 1991 1992 1994 1995 2000 2002 2005 2013 2015 2017
+  start=lubridate::ymd(c('1991-4-1',
+                         '1992-3-1',
+                         '1993-8-1',
+                         '1995-4-1',
+                         '2000-2-1',
+                         '2002-1-1',
+                         '2005-4-1',
+                         '2013-1-1',
+                         '2015-1-1',
+                         '2017-2-1')),
+  end=lubridate::ymd(c('1992-5-1',
+                        '1993-8-1',
+                        '1995-2-1',
+                        '1996-7-1',
+                        '2001-7-1',
+                        '2003-6-15',
+                        '2006-6-1',
+                        '2015-1-1',
+                        '2016-8-1',
+                        '2018-7-1'))
+)
+
+# create plot-able dataframes
+plot_ec <- ec_outputs[,names(ec_outputs) %in% c('Time', ec_eval_locs)]
+slct_ec <- plot_ec
+mod_ec <- plot_ec
+mod_ec$year <- NA
+slct_dates <- rep(FALSE, nrow(plot_ec))
+mod_dates <- rep(FALSE, nrow(plot_ec))
+for (i in 1:nrow(mod_pers)) {
+  slct_dates <- slct_dates | (plot_ec$Time >= slct_pers$start[i] & plot_ec$Time <= slct_pers$end[i])
+  mod_dates <- mod_dates | (plot_ec$Time >= mod_pers$start[i] & plot_ec$Time <= mod_pers$end[i])
+  mod_ec[(plot_ec$Time >= mod_pers$start[i] & plot_ec$Time <= mod_pers$end[i]), 'year'] <- mod_pers$year[i]
+}
+slct_ec[!slct_dates, -1] <- NA  # Exclude datetime column when replacing
+mod_ec[!mod_dates, !(names(mod_ec) %in% c('Time','year'))] <- NA  # Exclude datetime column when replacing
+mod_ec <- mod_ec[complete.cases(mod_ec),]
+mod_ec$year <- as.factor(mod_ec$year)
+
+plot_ec <- melt(plot_ec, id.vars='Time')
+slct_ec <- melt(slct_ec, id.vars='Time')
+mod_ec <- melt(mod_ec, id.vars=c('Time','year'))
+
+ds.date.lims <- c(min(plot_ec$Time),
+                  max(plot_ec$Time))
+
+# melt_ec <- melt(ec_outputs, id.vars='Time')
+# ggplot() +
+#   geom_line(data=melt_ec, mapping=aes(x=Time,y=value), linewidth=0.5, alpha=0.5, show.legend=FALSE) +
+#   facet_wrap(~variable, ncol=2, scales='free_y')
+
+plt <- ggplot() +
+  geom_line(data=plot_ec, mapping=aes(x=Time,y=value), linewidth=0.5, alpha=0.5, show.legend=FALSE) +
+  geom_line(data=mod_ec, mapping=aes(x=Time,y=value,color=year), linewidth=1) +
+  geom_line(data=slct_ec, mapping=aes(x=Time,y=value,color='selected cluster period'), linewidth=1, color='black', alpha=0.5) +
+  facet_wrap(~variable, ncol=1, scales='free_y') +
+  scale_x_date(breaks=date_breaks('6 months')) +
+  xlab("") +
+  ylab("EC (micro-Siemens/cm)") +
+  theme(text=element_text(size=12),
+        panel.border=element_rect(colour = "black", fill=NA, linewidth=0.5),
+        panel.background = element_blank(),
+        legend.key=element_blank(),
+        panel.grid.major.y = element_line(linewidth=.25, colour='grey80', linetype = 'dashed'),
+        panel.grid.major.x = element_line(linewidth=.25, colour='grey80', linetype = 'solid'),
+        panel.grid.minor.x = element_line(linewidth=.25, colour='grey90', linetype = 'dashed'),
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(angle=90),
+        legend.position.inside=c(0.975,0.1),
+        legend.justification=c(0.975,0.975),
+        legend.spacing=unit(c(0,0,0,0),"null"),
+        legend.background = element_rect(fill = "white", color = NULL),
+        legend.title=element_blank(),
+        axis.title.y = element_text(color='black')) 
+
+
+plt
+
+pltl <- ggplotly(plt, tooltip=c('value','Time','year'), dynamicTicks=TRUE)
+pltl
+
+pltl_name <- paste0(nclust,"_cluster_wy_slct_.html")
+
+saveWidget(pltl, pltl_name, selfcontained=TRUE)
+file.rename(pltl_name, paste0("plots/",pltl_name))
--- scripts/boundary_generation/2n_generate_latinhypercube_sets.R
+++ scripts/boundary_generation/2n_generate_latinhypercube_sets.R
@@ -3,36 +3,126 @@
 
 library(lhs)
 
+
+# define convert_to_numeric fxn -------------------------------------------
+
+# Function to convert to a numeric scale
+convert_to_numeric <- function(column) {
+  # Assign numeric codes (starting from 0 for the first level)
+  numeric_column <- seq(1,length(column))
+  numeric_column[is.na(column)] <- NA
+  
+  # Rescale to 0-1 if there are more than one unique values
+  if (length(unique(na.omit(column))) > 1) {
+    numeric_column <- (numeric_column - min(numeric_column, na.rm = TRUE)) / 
+      max(numeric_column, na.rm = TRUE)
+  }
+  
+  numeric_column[numeric_column>.99 | is.na(numeric_column)] <- 99
+  
+  return(numeric_column)
+}
 # Set wd to current file --------------------------------------------------
 
 rm(list=ls(all=TRUE)) #start with empty workspace
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Setworking directory to this file's directory
 
+# input data --------------------------------------------------------------
 
-pert_df <- data.frame(year=c(0,.25,.5,.75,99),
-                      tide=c(0,.5,99,99,99),
-                      dcc=c(0,.4,.6,.8,99),
-                      flows=c(0,0.3,0.8,99,99),
-                      dcd=c(0,1/3,2/3,99,99),
-                      suis=c(0,1/3,2/3,99,99))
-pert_desc <- data.frame(year=c(2008,2010,2012,2014),
-                        tide=c('Regular','Shifted + 100d',NA,NA),
-                        dcc=c('Regular','Perturbed','Always Open','Always Closed'),
-                        flows=c('Regular','Perturbed','Sacramento + 10%',NA),
-                        dcd=c('Regular','Perturbed - v1','Perturbed - v2',NA),
-                        suis=c('Regular','Perturbed - v1', 'Perturbed -v2',NA))
+seed <- 916
+nLHScol <- 15
+nLHSsamples <- 100
+
+version <- 'v4'
+
+years <- c('1991','1992','1994','1995','2000','2002','2005','2013','2015','2017')
+
+
+pert_desc <- data.frame(year=years,
+                        tide=c('Regular','Shifted + 100d', 'Shifted - 50d','Subtidal Pert',
+                               rep(NA, length(years)-4)),
+                        dcc=c('Regular','Perturbed','Always Open','Always Closed',
+                              rep(NA, length(years)-4)),
+                        flows=c('Regular','Perturbed v1','Perturbed v2',
+                                'Sacramento + 10%','Sacramento - 10%',
+                                rep(NA, length(years)-5)),
+                        dcd=c('Regular','Perturbed - v1','Perturbed - v2',
+                              rep(NA, length(years)-3)),
+                        suis=c('Regular','Perturbed - v1', 'Perturbed -v2',
+                               rep(NA, length(years)-3)))
+pert_df <- as.data.frame(lapply(pert_desc, convert_to_numeric))
+pert_df <- rbind(pert_df, rep(99, ncol(pert_df)))
 
 # Check setup
 names(pert_df) == names(pert_desc)
 
-seed <- 1
-nLHScol <- 15
-nLHSsamples <- 7
-
-version <- 'v3'
 
 
-# Run ---------------------------------------------------------------------
+# Run with no matching criteria -------------------------------------------------------
+
+
+set.seed(seed)
+num.df <- randomLHS(nLHSsamples,nLHScol)
+dup_rows <- TRUE
+
+while (dup_rows) {
+
+  res_df <- data.frame(matrix(NA,
+                              nrow <- nLHSsamples,
+                              ncol <- ncol(pert_desc)))
+  names(res_df) <- names(pert_desc)
+  rownames(res_df) <- seq(1,nLHSsamples)# rownames(target.df)
+  
+  # Determine result from LHC in english (not numbers)
+  for (c in seq(1,ncol(res_df))) {
+    for (r in seq(1,nLHSsamples)) {
+      cell <- num.df[r,c]
+      pert.col <- pert_df[,c]
+      for (n in seq(1,length(pert.col))) {
+        if (cell>=pert.col[n] & cell<pert.col[n+1]) {
+          res_df[r,c] <- pert_desc[n,c]
+          break
+        } # end if
+      } # end pert check
+    } # end row loop
+  } # end col loop
+  
+  res_df
+  
+  # Check for duplicated rows
+  any_duplicates <- any(duplicated(res_df))
+  
+  # Print the result
+  if (any_duplicates) {
+    print("There are duplicated rows.")
+    seed <- seed + 1
+    num.df <- randomLHS(nLHSsamples,nLHScol)
+    dup_rows <- TRUE
+    # Optionally, show which rows are duplicates
+    # duplicated_rows <- duplicated(res_df) | duplicated(res_df, fromLast = TRUE)
+    # print(res_df[duplicated_rows, ])
+  } else {
+    print(paste0("No duplicated rows found. Seed = ", seed))
+    dup_rows <- FALSE
+  }
+  
+
+}
+
+# Calculate percentages for each column
+percentage_list <- lapply(res_df, function(column) {
+  value_counts <- table(column)                # Count occurrences
+  percentages <- (value_counts / nrow(res_df)) * 100  # Calculate percentages
+  percentages
+})
+
+# Display percentages for each column
+percentage_list
+
+
+write.csv(res_df, paste0("./data_out/lhc_",version,".csv"), row.names=FALSE)
+
+# Run (look for matching criteria) -------------------------------------------------------
 match <- FALSE
 set.seed(seed)
 num.df <- randomLHS(nLHSsamples,nLHScol)
@@ -83,7 +173,7 @@ while (!match) {
 seed # ends up being 113
 res_df
 
-write.csv(res_df, paste0("./data_out/lhc_",version,".csv"), row.names=FALSE)
+# write.csv(res_df, paste0("./data_out/lhc_",version,".csv"), row.names=FALSE)
 
 # for adding rows to existing LHS use:
 # b <- augmentLHS(num.df,m=2)
