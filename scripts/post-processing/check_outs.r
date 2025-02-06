f3ebab8037066350c8227bb7c6b0d9a3947375b3
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-02-05 16:03:13 -0800

    compare dsm2 to schism outputs for cases 1001-1007

--- scripts/post-processing/check_outs.r
+++ scripts/post-processing/check_outs.r
@@ -23,7 +23,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Setworking directo
 casanntra.dir <- '../../../casanntra/data'
 # data.dir <- '../ann_training/data_out'
 
-cases <- seq(1,100)
+cases <- c(seq(1,107), seq(1001,1007))
 
 for (case in cases) {
   df <- read.csv(paste0(casanntra.dir,'/dsm2_base_',case,'.csv'))
@@ -39,6 +39,10 @@ for (case in cases) {
 }
 x2_ndo_df$case <- as.factor(x2_ndo_df$case)
 
+
+# Plot NDO ----------------------------------------------------------------
+
+
 plt <- ggplot(x2_ndo_df, aes(x=ndo, y=x2, color=case)) +
   geom_point()
 
@@ -50,36 +54,75 @@ ggplotly(plt)
 # check specific cases ----------------------------------------------------
 
 # cases in the 90s look like there's an anomoly for high ndo and high x2 (expected inverse relationship)
+for (case in seq(91,100)) {
+  df <- read.csv(paste0(casanntra.dir,'/dsm2_base_',case,'.csv'))
+  df$datetime <- lubridate::ymd(df$datetime)
+  
+  if (case==91) {
+    dsm2_check_df <- df
+  } else {
+    dsm2_check_df <- rbind(dsm2_check_df, df)
+  }
+}
+plt_cases <- dsm2_check_df[dsm2_check_df$case %in% seq(91,100),c('datetime','case',"mrz_tidal_energy", "mrz_tidal_filter", 
+                                                                 'sf_tidal_energy','sf_tidal_filter')]
+plt_cases <- melt(plt_cases, id.vars=c('datetime','case'))
+plt_cases$case <- as.factor(plt_cases$case)
 
-plt_cases <- x2_ndo_df[x2_ndo_df$case %in% seq(91,100),]
-# plt_cases <- x2_ndo_df
-
-plt <- ggplot(plt_cases, aes(x=datetime, y=x2, color=case)) +
+plt <- ggplot(plt_cases, aes(x=datetime, y=value, color=case)) +
+  facet_wrap(~variable, ncol=1, scales='free_y') +
   geom_line()
 
-plt
+# plt
 
-ggplotly(plt)
+pltl <- ggplotly(plt, dynamicTicks=TRUE)
 
-# the anomoly was shown in the first 30 days. Looking at the model spinup, this is a remnant of high salinity
+pltl_name <- paste0("check_dsm2_91-100.html")
+
+saveWidget(pltl, pltl_name, selfcontained=TRUE)
+file.rename(pltl_name, paste0("plots/",pltl_name))
+# the anomoly was shown in the first 30 days. This is not model spinup....
 
 
 # Plot a few EC out locs --------------------------------------------------
 
 
-cases <- c(1,11,21,31,41,51,61,71,81,91)
-# cases <- c(1,11,21,31,41,51)
-# cases <- seq(1,10)
-cases <- c(23,45)
-cases <- c(23,45,seq(101,107))
-cases <- c(23,seq(101,104))
-cases <- c(45,seq(105,107))
-
+# cases <- c(1,11,21,31,41,51,61,71,81,91)
+# cases <- c(23,45,seq(101,107))
+# cases <- c(23,seq(101,104))
+# cases <- c(45,seq(105,107))
+# good test for smcg
+cases <- c(5,6,9, # 6 & 9 same except for smscg ops and 5 is diff from both
+           11,12,13,
+           21,22,23,
+           37,38,39,
+           45,46,47,
+           53,54,55,
+           64,65,66,
+           76,78,79,
+           82,83,84,
+           96,97,98,
+           seq(1001,1007))  
+color_palette <- c(
+  "#E41A1C", "#377EB8", "#4DAF4A",
+  "#FF7F00", "#FFFF33", "#A65628",
+  "#984EA3", "#F781BF", "#999999",
+  "#66C2A5", "#FC8D62", "#8DA0CB",
+  "#E78AC3", "#A6D854", "#FFD92F",
+  "#B3B3B3", "#1B9E77", "#D95F02",
+  "#7570B3", "#E7298A", "#66A61E",
+  "#A6761D", "#666666", "#17BECF",
+  "#BCBD22", "#D62728", "#9467BD",
+  "#2CA02C", "#FF7F0E", "#1F77B4",
+  "#E41A1C", "#377EB8", "#4DAF4A",
+  "#FF7F00", "#FFFF33", "#A65628",
+  "#984EA3", "#F781BF", "#999999",
+  "#66C2A5", "#FC8D62", "#8DA0CB"
+)
 for (case in cases) {
   df <- read.csv(paste0(casanntra.dir,'/dsm2_base_',case,'.csv'))
   df$datetime <- lubridate::ymd(df$datetime)
-  df <- df[,c('datetime','case',"mrz_tidal_energy", "mrz_tidal_filter", 
-              'sf_tidal_energy','sf_tidal_filter')] # subset variables
+  df <- df[,c('datetime','case',"mrz_tidal_filter",'dcc','northern_flow','cu_flow','smscg','emm2')] # subset variables
   # df <- df[,c('datetime','case','ndo', 'cu_flow', 'sjr_flow','sf_tidal_energy','sf_tidal_filter',"mrz_tidal_energy", "mrz_tidal_filter",'emm2')] # subset variables
   
   if (case==cases[1]) {
@@ -95,6 +138,10 @@ plt <- ggplot(var_df, aes(x=datetime, y=value, color=variable)) +
   facet_wrap(~case, ncol=1, scales='free_y') +
   geom_line()
 
+plt <- ggplot(var_df, aes(x=datetime, y=value, color=case)) +
+  facet_wrap(~variable, ncol=1, scales='free_y') +
+  scale_color_manual(values = color_palette) +
+  geom_line()
 # plt
 
 ggplotly(plt, dynamicTicks=TRUE)
@@ -103,3 +150,93 @@ ggplotly(plt, dynamicTicks=TRUE)
 #   geom_line()
 # 
 # ggplotly(plt)
+
+
+
+# Compare SCHISM to DSM2 --------------------------------------------------
+
+casanntra.dir <- '../../../casanntra/data'
+
+# SCHISM
+sch_cases <- seq(1,7)
+# sch_cases <- c(1)
+
+for (case in sch_cases) {
+  df <- read.csv(paste0(casanntra.dir,'/schism_base_',case,'.csv'))
+  df$datetime <- lubridate::ymd(df$datetime)
+  df$case <- case + 1000
+  
+  if (case==1) {
+    sch_df <- df
+  } else {
+    sch_df <- rbind(sch_df, df)
+  }
+}
+
+# DSM2
+dsm2_cases <- seq(1001,1007)
+# dsm2_cases <- c(1001)
+
+for (case in dsm2_cases) {
+  df <- read.csv(paste0(casanntra.dir,'/dsm2_base_',case,'.csv'))
+  df$datetime <- lubridate::ymd(df$datetime)
+  df <- df[, setdiff(names(df), "northern_flow")]
+  
+  if (case==1001) {
+    dsm2_df <- df
+  } else {
+    dsm2_df <- rbind(dsm2_df, df)
+  }
+}
+
+compare_std <- c("datetime", "model", "scene", "case")
+compare_flux <- c("sac_flow", "sjr_flow", "exports", "ndo", "cu_flow")
+compare_tide <- c("sf_tidal_energy","sf_tidal_filter")
+compare_x2 <- c("x2")
+compare_gates <- c("smscg","dcc")
+compare_ec <- c('emm2','rsl','jer','bdl','cse') #,'anh','mal','vcu','wci','tms','anh','cll')
+#c("trp", "wci", "vcu", "uni", "rsl", "old", "pct", "mal", "cll", "emm2", "srv", "anc", "jer", "sal", "ppt", "rri2", "bdt", "lps", "snc", "dsj", "bdl", "nsl2", "vol", "tss", "sss", "tms", "anh", "oh4", "rsl.1", "vcu.1", "mtz")
+all_vars <- c(compare_std, compare_flux, compare_tide, 
+              compare_x2, compare_gates, compare_ec)
+
+dsm2_df <- dsm2_df[,names(dsm2_df) %in% all_vars]
+sch_df <- sch_df[,names(sch_df) %in% all_vars]
+comb.df <- rbind(dsm2_df, sch_df)
+comb.df <- melt(comb.df, id.vars=compare_std)
+
+
+compare_cases <- seq(1001,1007)
+
+for (case in compare_cases) {
+  
+  # plot
+  plt.df <- comb.df[comb.df$case == case, ]
+  
+  plt <- ggplot(data=plt.df) + 
+    geom_line(aes(x=datetime, y=value, color=model)) +
+    facet_wrap(~variable, ncol=2, scales='free_y') +
+    scale_x_date(date_breaks = "6 month", date_labels = "%b-%Y", expand=c(0,0)) +
+    theme(text=element_text(size=12),
+          axis.text.x = element_text(angle=90),
+          panel.border=element_rect(colour = "black", fill=NA, linewidth=0.5),
+          panel.background = element_blank(),
+          legend.key=element_blank(),
+          panel.grid.major.y = element_line(linewidth=.25, colour='grey80', linetype = 'dashed'),
+          axis.line = element_line(colour = "black"),
+          legend.position.inside=c(0.975,0.975),
+          legend.justification=c(0.975,0.975),
+          legend.spacing=unit(c(0,0,0,0),"null"),
+          legend.background = element_rect(fill = "white", color = NULL),
+          legend.title=element_blank(),
+          axis.title.y = element_text(color='black')
+    )
+  
+  pltl <- ggplotly(plt, dynamicTicks=TRUE)
+  
+  pltl_name <- paste0("check_dsm2_v_schism_",case,".html")
+  
+  saveWidget(pltl, pltl_name, selfcontained=TRUE)
+  file.rename(pltl_name, paste0("plots/",pltl_name))
+  
+} 
+
